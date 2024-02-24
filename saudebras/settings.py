d37a759c3526d1f34a2ca59c6f8d80acd2c73247
Augusto215 <augusto.dataanalysis@gmail.com>
2024-02-24 07:26:57 -0300

    novo122

--- saudebras/settings.py
+++ saudebras/settings.py
@@ -93,7 +93,7 @@ ROOT_URLCONF = 'saudebras.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'saudebras/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
