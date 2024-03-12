4e1ad71d3916d8b2b9b92429870f9e978786a777
Augusto215 <augusto.dataanalysis@gmail.com>
2024-03-12 12:44:01 -0300

    novo124

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(form_class=EmailPasswordResetForm), name='password_reset'),
 
     # Outras URLs do processo de redefinição de senha
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/reset_done.html'), name='password_reset_complete'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),    
     
