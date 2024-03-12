e876b4fd1acf1f975eb338bd519899a93d0c282e
Augusto215 <augusto.dataanalysis@gmail.com>
2024-03-12 12:57:46 -0300

    novo124

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/reset_done.html'), name='password_reset_complete'),
     
-    
 ]
 
 
